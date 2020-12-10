aa426518d4ee738ab2559080bf61ba19221be814
Jesse Langford <jesselangford472@gmail.com>
2020-12-10 20:37:51 +0800

    updated webpack source map loader

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,4 @@ import * as ReactDom from "react-dom"
 
 import { App } from "./app"
 
-///<reference types="webpack-env" />
-
-ReactDom.render(<App />, document.getElementById("root"));
-
-if (module.hot) {
-  module.hot.accept();
-}
+ReactDom.render(<App />, document.getElementById("root"))
